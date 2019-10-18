866b48c08b7a093e87695b954535d54816419fbf
Thomas Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-10-18 11:58:44 -0400

    Revert "Ring buffer"

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,12 +4,8 @@ class RingBuffer:
     self.current = 0
     self.storage = [None]*capacity
 
-  def append(self, item): 
-    self.storage[self.current] = item         # Stores the current input item
-    if self.current + 1 > self.capacity - 1:   # If the current set of items is more than the
-        self.current = 0                       # capacity replace current, if not add it.
-    else:
-        self.current += 1
+  def append(self, item):
+    pass
 
   def get(self):
-    return [value for value in self.storage if value is not None] # returns a value if its there
+    pass
